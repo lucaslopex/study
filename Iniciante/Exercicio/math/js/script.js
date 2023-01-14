87c6fa0bcf02d503bce1091f8f615329dd11b5a4
Lucas Lopes Silva <lopes.lucassilva@hotmail.com>
2023-01-13 22:02:57 -0300

    math1

---
+++ Iniciante/Exercicio/math/js/script.js
@@ -0,0 +1,5 @@
+const numero = prompt('Digite um número para consulta: ')
+const num_html = document.getElementById('num-principal')
+const texto = document.getElementById('js')
+
+num_html.innerHTML = numero
