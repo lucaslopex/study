f01760737e71defeaee21656749c1ed30211885e
Lucas Silva <lucaslopessilva17@gmail.com>
2023-01-30 22:12:45 -0300

    exclusão do math

--- Iniciante/Exercicio/math/js/script.js
+++
@@ -1,5 +0,0 @@
-const numero = prompt('Digite um número para consulta: ')
-const num_html = document.getElementById('num-principal')
-const texto = document.getElementById('js')
-
-num_html.innerHTML = numero
